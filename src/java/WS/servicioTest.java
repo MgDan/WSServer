50d4124c2edb3e306ebb47e39ace14b2d46da6c3
MgDan <danielvzzesc@gmail.com>
2017-11-19 20:12:00 -0600

    web service server ejemplo

---
+++ src/java/WS/servicioTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WS;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author DanVz
+ */
+@WebService(serviceName = "servicioTest")
+public class servicioTest {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "suma")
+    public int suma(@WebParam(name = "numero1") int numero1, @WebParam(name = "numero2") int numero2) {
+        //TODO write your implementation code here:
+        return numero1 + numero2;
+    }
+
+}
